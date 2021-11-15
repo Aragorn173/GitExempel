60442cbbcef0bd1b086c5ee44cb285a08d8ee316
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2021-11-15 11:19:31 +0100

    started the project

--- src/GitExempel.java
+++ src/GitExempel.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class GitExempel {
     public static void main(String[] args) {
-        
+        int antal = Integer.parseInt(JOptionPane.showInputDialog("Antal siffror?"));
+        int[] siffror = new int [antal];
+        int i = 1;
+        while(i<antal){
+            int[i] siffror = JOptionPane.showInputDialog("tal nummer" + i + "?");
+             i = i + 1;
+
+        }
     }
 }
