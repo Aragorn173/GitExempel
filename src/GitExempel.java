992448e4692d289e2b0e4a724e2d0819d79a9140
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2021-11-16 15:04:44 +0100

    calculated the average

--- src/GitExempel.java
+++ src/GitExempel.java
@@ -4,11 +4,17 @@ public class GitExempel {
     public static void main(String[] args) {
         int antal = Integer.parseInt(JOptionPane.showInputDialog("Antal siffror?"));
         int[] siffror = new int [antal];
-        int i = 1;
+        int i = 0;
+        double average = 0;
+        int e = 1;
         while(i<antal){
-            int[i] siffror = JOptionPane.showInputDialog("tal nummer" + i + "?");
+             siffror[i] = Integer.parseInt(JOptionPane.showInputDialog("tal nummer " + e + "?"));
+             average = average + siffror[i];
              i = i + 1;
-
+             e = e + 1;
         }
+        average = average / antal;
+        System.out.println("medelvärdet på dina " + antal + " tal är" + average);
+
     }
 }
